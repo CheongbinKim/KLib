eb90f1349efaa97a2740125f1f82507499146831
kev1n <cheongbinkim@gmail.com>
2018-05-20 19:21:53 +0900

    puzzle

--- KMain.cpp
+++ KMain.cpp
@@ -7,251 +7,123 @@
 
 #define RECT_WIDTH 100
 #define RECT_HEIGHT 100
+#define MAX 3
+#define DIR_LEFT 0
+#define DIR_RIGHT 1
+#define DIR_UP 2
+#define DIR_DOWN 3
 
 int mx;
 int my;
-int turn = 0; 
-int com_bingo=0;
-int user_bingo=0;
-
 HWND mHwnd;
-
+int puzzle[MAX][MAX] = { 0 };
 
 #pragma warning(disable:4996)
+void initArray();
+int changeElement(int sx, int sy, int dx, int dy);
+void shakeArray();
+void startNewGame();
 
-
-#define MAX 5
-int user[MAX][MAX] = { 0 };
-int com[MAX][MAX] = { 0 };
-
-int flag = 1;
-
-void GameStart();
-void InitArray(int arr[][MAX]);
-void UserInput(int n);
-void ComInput();
-int Check(int arr[][MAX]);
-void Choose(int n);
-void ChooseItem(int arr[][MAX], int v);
-void CheckAll();
-void OnDraw(HDC hdc);
-void OnLButtonDown(HWND hWnd, LPARAM lParam);
-void OnKeyDown(HWND hWnd, WPARAM wParam);
-
-
-
-void GameStart()
+void startNewGame()
 {
-	InitArray(user);
-
-	InitArray(com);
+	initArray();
+	//shakeArray();
 }
 
-
-void InitArray(int arr[][MAX])
+void initArray()
 {
-	int temp[MAX*MAX];
-
-	for (int i = 0; i < MAX*MAX; i++)
-	{
-		int ran = rand() % (MAX*MAX * 2) + 1;
-
-		temp[i] = ran;
-
-		for (int j = 0; j < i; j++)
-		{
-			if (temp[j] == ran)
-			{
-				i--;
-				break;
-			}
-		}
-	}
-
+	int a = 1;
 	for (int y = 0; y < MAX; y++)
 	{
 		for (int x = 0; x < MAX; x++)
 		{
-			arr[y][x] = temp[MAX * y + x];
+			puzzle[y][x] = a++;
 		}
 	}
-
-	arr[MAX / 2][MAX / 2] = -1;
 }
 
-
-void ChooseItem(int arr[][MAX], int v)
+int changeElement(int sx, int sy, int dx, int dy)
 {
-	for (int y = 0; y < MAX; y++)
+	if (0 > sx || 0 > sy || 0 > dx || 0 > dy)
 	{
-		for (int x = 0; x < MAX; x++)
+		return -1;
+	}
+	
+	if (MAX <= sx || MAX <= sy || MAX <= dx || MAX <= dy)
+	{
+		return -1;
+	}
+
+	if (9 == puzzle[dy][dx])
+	{
+		int t = puzzle[sy][sx];
+		puzzle[sy][sx] = puzzle[dy][dx];
+		puzzle[dy][dx] = t;
+	}
+
+	return 0;
+}
+
+void shakeArray()
+{
+	int x = MAX-1;
+	int y = MAX-1;
+	for (int i = 0; i < 1000; i++)
+	{
+		int ran = rand() % 4;
+
+		switch (ran)
 		{
-			if (arr[y][x] == v)
+		case DIR_LEFT:
+			if (0 == changeElement(x, y, x - 1, y))
 			{
-				arr[y][x] = -1;
-				break;
+				x--;
 			}
-		}
-	}
-}
-
-
-
-void UserInput(int n)
-{
-	Choose(n);
-	// user 빙고 체크
-	CheckAll();
-	// com 값 입력
-	turn = 1;
-	// com, user 아이템 선택
-	ComInput();
-	// 빙고 체크
-	CheckAll();
-	turn = 0;
-}
-
-
-void CheckAll()
-{
-	user_bingo = Check(user);
-	com_bingo = Check(com);
-}
-
-
-void ComInput()
-{
-	int ran = 0;
-
-	int y, x;
-
-	for (;;)
-	{
-		ran = rand() % (MAX*MAX) + 1;
-		y = ran / MAX;
-		x = ran % MAX;
-
-		if (com[y][x] == -1)
-			continue;
-		else
 			break;
-	}
-
-
-
-	Choose(com[y][x]);
-}
-
-
-int Check(int arr[][MAX])
-{
-	int temp = 0;
-
-	for (int y = 0; y < MAX; y++)
-	{
-		int cmd = 0;
-
-		for (int x = 0; x < MAX; x++)
-		{
-			if (arr[y][x] == -1)
+		case DIR_RIGHT:
+			if (0 == changeElement(x, y, x + 1, y))
 			{
-				cmd++;
+				x++;
 			}
-		}
-		if (cmd == MAX)
-		{
-			temp++;
-		}
-	}
-
-	for (int x = 0; x < MAX; x++)
-	{
-		int cmd = 0;
-
-		for (int y = 0; y < MAX; y++)
-		{
-			if (arr[y][x] == -1)
+			break;
+		case DIR_UP:
+			if (0 == changeElement(x, y, x, y - 1))
 			{
-				cmd++;
+				y--;
 			}
-		}
-		if (cmd == MAX)
-		{
-			temp++;
-		}
-	}
-
-
-	int cmd = 0;
-	for (int i = 0; i < MAX; i++)
-	{
-		if (arr[i][MAX - i - 1] == -1)
-		{
-			cmd++;
-		}
-		if (cmd == MAX)
-		{
-			temp++;
-		}
-	}
-
-
-	cmd = 0;
-	for (int i = 0; i < MAX; i++)
-	{
-
-		if (arr[i][i] == -1)
-		{
-			cmd++;
-		}
-		if (cmd == MAX)
-		{
-			temp++;
-		}
-	}
-
-	return temp;
-}
-
-
-void Choose(int n)
-{
-	ChooseItem(user, n);
-	ChooseItem(com, n);
-}
-
-
-void PrintAPI(HDC hdc, int arr[][MAX],int sx,int sy)
-{
-	for (int y = 0; y < MAX; y++)
-	{
-		for (int x = 0; x < MAX; x++)
-		{
-			int dx = x*RECT_WIDTH+sx;
-			int dy = y*RECT_HEIGHT+sy;
-
-			if (arr[y][x] != -1)
+			break;
+		case DIR_DOWN:
+			if (0 == changeElement(x, y, x, y + 1))
 			{
-				char msg[20];
-				wsprintf(msg, "%d", arr[y][x]);
-				TextOutA(hdc, dx + 40, dy + 40, msg, strlen(msg));
+				y++;
 			}
+			break;
 		}
 	}
 }
-
-
-//int main()
-//{
-//	srand(time(NULL));
-//	GameStart(hdc);
-//}
 
 void OnCreate(HWND hWnd)
 {
 	mHwnd = hWnd;
 	srand(time(NULL));
-	GameStart();
+	startNewGame();
+}
+
+int gameOver()
+{
+	int a = 1;
+	for (int y = 0; y < MAX; y++)
+	{
+		for (int x = 0; x < MAX; x++)
+		{
+			if (a != puzzle[y][x])
+			{
+				return -1;
+			}
+			a++;
+		}
+	}
+	return 0;
 }
 
 
@@ -261,54 +133,59 @@ void OnDraw(HDC hdc)
 	{
 		for (int x = 0; x < MAX; x++)
 		{
-			if (-1 == user[y][x])
+			Rectangle(hdc, RECT_WIDTH * x, RECT_HEIGHT * y, RECT_WIDTH * x + RECT_WIDTH, RECT_HEIGHT * y + RECT_HEIGHT);
+		}
+	}
+
+	for (int y = 0; y < MAX; y++)
+	{
+		for (int x = 0; x < MAX; x++)
+		{
+			if (9 != puzzle[y][x]) 
 			{
-				HBRUSH hBrush,oBrush;
-				hBrush = CreateSolidBrush(RGB(255, 0, 0));
-				oBrush = (HBRUSH)SelectObject(hdc, hBrush);
-				Rectangle(hdc, 100 * x, 100 * y, 100 * x + 100, 100 * y + 100);
-				SelectObject(hdc, oBrush);
-				DeleteObject(hBrush);
-			}
-			else
-			{
-				Rectangle(hdc, 100 * x, 100 * y, 100 * x + 100, 100 * y + 100);
+				char msg[20];
+				wsprintf(msg, "%d", puzzle[y][x]);
+				TextOutA(hdc, 100 * x + 50, 100 * y + 50, msg, strlen(msg));
 			}
 			
 		}
 	}
-
-	PrintAPI(hdc, user,0,0);
-	PrintAPI(hdc, com, 700, 0);
-	
-	char msg[20];
-	wsprintf(msg, "%d,%d", mx, my);
-	TextOutA(hdc, 600, 10, msg, strlen(msg));
-
-	wsprintf(msg, "user bingo : %d",user_bingo);
-	TextOutA(hdc, 600, 40, msg, strlen(msg));
-
-	wsprintf(msg, "com bingo : %d",com_bingo);
-	TextOutA(hdc, 600, 70, msg, strlen(msg));
-
-	wsprintf(msg, "turn : %d", turn);
-	TextOutA(hdc, 600, 100, msg, strlen(msg));
 }
 
-void OnLButtonDown(HWND hWnd,LPARAM lParam)
+void OnLButtonDown(HWND hWnd, LPARAM lParam)
 {
 	int lx = LOWORD(lParam);
 	int ly = HIWORD(lParam);
 
-	mx = lx;
-	my = ly;
+	if (lx > MAX*RECT_WIDTH || ly > MAX*RECT_HEIGHT)
+	{
+		return;
+	}
 
-	int n = user[my / RECT_WIDTH][mx / RECT_HEIGHT];
+	int x = lx / RECT_WIDTH;
+	int y = ly / RECT_WIDTH;
 
+	if (0 == changeElement(x, y, x - 1, y))
+	{
 
-	UserInput(n);
+	}
+	if (0 == changeElement(x, y, x + 1, y))
+	{
+	}
+	if (0 == changeElement(x, y, x, y - 1))
+	{
+	}
+	if (0 == changeElement(x, y, x, y + 1))
+	{
+	}
 
-	InvalidateRect(hWnd,NULL,true);
+	// 완성되었는지?
+	if (0 == gameOver())
+	{
+		shakeArray();
+	}
+
+	InvalidateRect(hWnd, NULL, true);
 }
 
 void OnKeyDown(HWND hWnd,WPARAM wParam)
@@ -322,6 +199,8 @@ void OnKeyDown(HWND hWnd,WPARAM wParam)
 	case VK_DOWN:
 		break;
 	case VK_UP:
+		shakeArray();
 		break;
 	}
+	InvalidateRect(hWnd, NULL, true);
 }
